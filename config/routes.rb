935b36652b83bccb6eabaf1b760fbd8e244b8736
EC2 Default User <ec2-user@ip-172-31-5-255.ap-northeast-1.compute.internal>
2023-08-29 07:46:36 +0000

    halfway7

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   get '/top' => 'homes#top'
+
   # .../lists/1 や .../lists/3 に該当する
   get 'lists/:id' => 'lists#show', as: 'list'
   patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists
 end
